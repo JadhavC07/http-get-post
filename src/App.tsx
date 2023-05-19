b0c2653bfd6b73252d0d39c9c99c696c04c84a98
JadhavC07 <cjaddhav@gmail.com>
2023-05-19 15:49:11 +0530

    https get post

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/users")
+      .then((res) => {
+        const { allUsers } = res.data;
+        console.log("allUsers from axios", allUsers);
+        setUsers(allUsers);
+      })
+      .catch((err) => {
+        console.log("err from axios", err.message);
+      });
+  }, []);
+
+  console.log("users", users);
+  const allUsersHTML = users.map((user: any, index) => {
+    return (
+      <p key={user.id}>
+        User-{index} := {user.email}
+      </p>
+    );
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>HTTP</p>
+      {allUsersHTML}
     </div>
   );
 }
